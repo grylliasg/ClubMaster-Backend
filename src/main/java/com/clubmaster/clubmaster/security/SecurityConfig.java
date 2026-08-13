768dc6da0ab307094be80e5bb041de7511f8521f
grylliag <grillias72@gmail.com>
2026-08-13 21:26:56 +0300

    Add role-based authorization closes #20

--- src/main/java/com/clubmaster/clubmaster/security/SecurityConfig.java
+++ src/main/java/com/clubmaster/clubmaster/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -47,7 +48,8 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**", "/error/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/teams/**", "/players/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 // 2. Προσθέτουμε το JWT φίλτρο μας
