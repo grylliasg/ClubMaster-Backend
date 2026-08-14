8aa5198150cf46ea2281f9ee75a28e5b11abe025
grylliag <grillias72@gmail.com>
2026-08-14 17:23:25 +0300

    Error handling for invalid format of LocalData at player date of birth (needs custom handler) closes #13

--- src/main/java/com/clubmaster/clubmaster/exception/GlobalExceptionHandler.java
+++ src/main/java/com/clubmaster/clubmaster/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.clubmaster.clubmaster.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,8 +20,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 
+    // Catch the error that @Valid caused
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid data provided");
+    }
+
+    // Invalid format for LocalDate
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
     }
 }
